e26e17d545b420f231d014682aa6f519bb64ff40
Radoslav Tsonev <radoslavtsonev01@gmail.com>
2022-03-24 00:18:33 +0200

    Revert "3"
    This reverts commit 9b7037c0df907670e65d67d2c500d55d54e17b70.

--- 23.Build a Translator.py
+++
@@ -1,8 +0,0 @@
-def translate(phrase):
-    translation = ""
-    for letter in phrase:
-        if letter in "AEIOUaeiou":
-            translation = translation + "g"
-        else:
-            translation = translation + letter
-        return translation

0ba6233a36fcd8f0e3a201d7d33568d2bcef5481
Radoslav Tsonev <radoslavtsonev01@gmail.com>
2022-03-25 23:55:01 +0200

    part 5

---
+++ 26.Reading Files.py
@@ -0,0 +1,4 @@
+
+employee_file = open("26.employees.txt", "r")
+print(employee_file.read())
+employee_file.close()
---
+++ 27.Writing to Files.py
@@ -0,0 +1,3 @@
+employee_file = open("27.index.html", "w")
+employee_file.write("<p>This is HTML</p>")
+employee_file.close()
